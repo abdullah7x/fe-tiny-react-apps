f5e12832244939aff2f4fad51d83ec772d466f6c
abdullah7x <abdullah.ahmed7x@gmail.com>
2022-05-22 14:05:26 +0100

    push todo list and noughts and crosses

--- xsnos/src/App.js
+++ xsnos/src/App.js
@@ -1,52 +1,11 @@
-import { useState } from 'react';
+import Board from './components/Board';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-      <h1>Naughts and Crosses</h1>
-      <div id="game-board">
-        <CreateBoard></CreateBoard>
-      </div>
+    <div>
+      <Board />
     </div>
   );
 }
-
-const CreateBoard = (props) => {
-  const [boxArray, setBoxArray] = useState([
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-  ]);
-  const [turn, setTurn] = useState(1);
-
-  const handleClick = (index) => {
-    setBoxArray((currArray) => {
-      const newArray = [...currArray];
-      if (turn % 2 !== 0) {
-        newArray[index] = 'X';
-      } else newArray[index] = 'O';
-      return newArray;
-    });
-    setTurn((currTurn) => {
-      const number = currTurn + 1;
-      return number;
-    });
-  };
-
-  return boxArray.map((box, index) => {
-    return (
-      <button className="box" key={index} onClick={() => handleClick(index)}>
-        {box}
-      </button>
-    );
-  });
-};
 
 export default App;
---
+++ xsnos/src/components/Board.jsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import { useState } from 'react';
+
+function Board() {
+  const [boxArray, setBoxArray] = useState([
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+  ]);
+  const [disabled, setDisabled] = useState([
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+  ]);
+  const [turn, setTurn] = useState(1);
+  const [overMessage, setOverMessage] = useState('');
+  const [gameOver, setGameOver] = useState(false);
+
+  const handleClick = (index) => {
+    setDisabled((currDisabled) => {
+      const disArray = [...currDisabled];
+      disArray[index] = true;
+      return disArray;
+    });
+    setTurn((currTurn) => {
+      const number = currTurn;
+      return number + 1;
+    });
+    setBoxArray((currBox) => {
+      const box = [...currBox];
+      if (turn % 2 !== 0) {
+        box[index] = 'O';
+      } else {
+        box[index] = 'X';
+      }
+
+      if (
+        (box[0] === 'O' && box[1] === 'O' && box[2] === 'O') ||
+        (box[0] === 'X' && box[1] === 'X' && box[2] === 'X') ||
+        (box[0] === 'X' && box[3] === 'X' && box[6] === 'X') ||
+        (box[0] === 'O' && box[3] === 'O' && box[6] === 'O') ||
+        (box[0] === 'X' && box[4] === 'X' && box[8] === 'X') ||
+        (box[0] === 'O' && box[4] === 'O' && box[8] === 'O') ||
+        (box[1] === 'X' && box[4] === 'X' && box[7] === 'X') ||
+        (box[1] === 'O' && box[4] === 'O' && box[7] === 'O') ||
+        (box[2] === 'X' && box[5] === 'X' && box[8] === 'X') ||
+        (box[2] === 'O' && box[5] === 'O' && box[8] === 'O') ||
+        (box[2] === 'X' && box[4] === 'X' && box[6] === 'X') ||
+        (box[2] === 'O' && box[4] === 'O' && box[6] === 'O') ||
+        (box[3] === 'X' && box[4] === 'X' && box[5] === 'X') ||
+        (box[3] === 'O' && box[4] === 'O' && box[5] === 'O') ||
+        (box[6] === 'X' && box[7] === 'X' && box[8] === 'X') ||
+        (box[6] === 'O' && box[7] === 'O' && box[8] === 'O')
+      ) {
+        setGameOver(true);
+        setTurn((n) => {
+          if (n % 2 === 0) setOverMessage('Game Over: Player O wins');
+          else setOverMessage('Game Over: Player X wins');
+        });
+      }
+      return box;
+    });
+  };
+
+  return (
+    <>
+      <h1>Naughts and Crosses</h1>
+      <h2>{overMessage}</h2>
+      <div id="game-board">
+        {boxArray.map((box, index) => {
+          return (
+            <button
+              disabled={gameOver ? 'disabled' : disabled[index]}
+              className="box"
+              key={index}
+              onClick={() => handleClick(index)}
+            >
+              <h1>{box}</h1>
+            </button>
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default Board;
